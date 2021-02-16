11b8fff27728f1e9b5d7f4f294ce877eab484fc2
hkg328 <hkg328@outlook.com>
2021-02-15 19:39:46 -0800

    add serializer

--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import main
+from .views import RoomView
 
 urlpatterns = [
-    path('home', main)
+    path('room', RoomView.as_view())
 ]
